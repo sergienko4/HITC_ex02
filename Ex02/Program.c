6f6d222fdc7d3c30c4ac65aa870507f0560b4bc7
sergienko4@gmail.com <sergienko4@gmail.com>
2017-12-22 21:44:52 +0200

    Added Ex 02

--- Ex02/Program.c
+++ Ex02/Program.c
@@ -1,44 +1,67 @@
 #include<stdio.h>
 
+
 void Ex01();
 int GetDef(int*, int);
 
+void Ex02();
+int RecursionEx02(int);
+
+
 void main(){
 
-	Ex01();
+	//Ex01();
+	//Ex02();
 }
 
 
 void Ex01(){
+
 	int length;
 	int arry[] = {-50,-10,-10,72,81,81,81,81,93,93};
 	length = sizeof(arry) / sizeof(int);
 	GetDef(arry, length);
+	//free(arry);
 
 }
 int GetDef(int* arraP, int length){
-	int counter = 0, i, replace = -1;
-	int to = length -1;
-	int *P2 = arraP;
+	// counter start on one because one number is must be.
+	int counter = 1, index = 0, poze = 0;
 
-	//double value mark as ZERO
-	for(i=0; i<to; i++){
-		if(arraP[i]==arraP[i+1]){
-			arraP[i+1]=arraP[i+2];
+	while(index<length){
+
+		if(arraP[poze] != arraP[index])
+		{
+			// update second index 
+			poze++;
+			// move to new position the new number
+			arraP[poze] = arraP[index];
+
+			// add counter
+			counter++;
 		}
 		else
-		{
-				counter ++;
-		}
+			index++;
 	}
 	return counter;
 }
 
-		////// sort the array
-		//for(i = 0; i< counter; i++)
-		//	if(arraP[i+2]==0){
-		//		arraP[i+1]=arraP[i+2];
-		//		arraP[i+2] = 0;
-		//	}
-		//	return counter;
-	//}
+void Ex02(){
+	int result =RecursionEx02(9999);
+}
+int RecursionEx02(int n){
+	int d, temp;
+
+	if(n<10) {
+		// get unit ++ and return new value
+		return n++%10;
+	}
+	else
+	{
+		d = RecursionEx02(n/10) *10;
+		// get unit
+		temp = (++n)%10;
+		// add to result number the new unit 
+		return  d + temp;
+	}
+}
